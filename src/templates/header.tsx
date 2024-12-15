2975d1ee2dbd453be97e1e8a02b074857b054b2e
James Latten <james.latten@phntm.com>
2024-12-15 16:07:16 -0500

    Fixing header

--- src/templates/header.tsx
+++ src/templates/header.tsx
@@ -14,39 +14,29 @@ const Header = () => {
         <header className="header-primary">
             <div className="inner-header container">
                 <div className="inner-header-row row">
-                    <div className="inner-header-nav col-lg-4">
+                    <div className="inner-header-nav col-lg-4 d-md-none">
                         <ul>
                             <li className={isActive('/')} >
                                 <Link href="/">
                                     Welcome
                                 </Link>    
                             </li>
-                            <li className={isActive('/services')}>
-                                <Link href="/services">
+                            <li className={isActive('#services')}>
+                                <Link href="#services">
                                     Services
-                                </Link>
-                            </li>
-                            <li className={isActive('/work')}>
-                                <Link href="/work">
-                                    Work
-                                </Link>
-                            </li>
-                            <li className={isActive('/about')}>
-                                <Link href="/about">
-                                    About
                                 </Link>
                             </li>
                         </ul>
                     </div>
-                    <div className="inner-header-branding col-lg-4">
+                    <div className="inner-header-branding col-lg-4 col-md-12">
                         <Link href="/">
                             <h2>Creative MSFTS</h2>
                             <p>EST. 2023</p>
                         </Link>
                     </div>
-                    <div className="inner-header-cta col-lg-4">
+                    <div className="inner-header-cta col-lg-4 d-md-none">
                         <ul>
-                            <li>
+{/*                             <li>
                                 <Link href="/subscriptions">
                                     Subscribe
                                 </Link>
@@ -54,6 +44,16 @@ const Header = () => {
                             <li>
                                 <Link href="/pricing">
                                     Pricing
+                                </Link>
+                            </li> */}
+                            <li className={isActive('#work')}>
+                                <Link href="#work">
+                                    Work
+                                </Link>
+                            </li>
+                            <li className={isActive('#about')}>
+                                <Link href="#about">
+                                    About
                                 </Link>
                             </li>
                         </ul>
